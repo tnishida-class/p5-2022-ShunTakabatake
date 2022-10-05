692dcef4435a35331d38141339f0d8836f8a5a53
ShunTakabatake <114969983+ShunTakabatake@users.noreply.github.com>
2022-10-05 11:42:07 +0900

    Updated 1-1
    I wanna try open sand!

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,16 +1,18 @@
 function setup() {
-  createCanvas(128,128);
+  createCanvas(256,160);
 }
 
 function draw() {
   background(0);
   strokeWeight(4);
-  fill(178, 100, 162);
+  fill(255);
+  triangle(24,132,126,132,126,20);
+  fill(255, 150, 200);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
+  fill(84, 170, 50);
   triangle(0,108,106,108,106,0);
   fill(255);
   textSize(32);
   textFont("serif");
-  text("46", 68, 100);
+  text("OpenSand", 68, 100);
 }
