487a0072e97b529139687149d7c5c91548a35647
ShunTakabatake <114969983+ShunTakabatake@users.noreply.github.com>
2022-10-07 09:35:24 +0900

    Updated 1-q1
    ロゴの色を緑にしたかったのですが、線ではなく内部が緑になってしまい、困りました。

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -4,11 +4,14 @@ function setup(){
   background(255);
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
+  
   noFill();
   strokeWeight(25);
   strokeCap(SQUARE);
+  //fill(18, 115, 44);
   arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
-  // BLANK[1]
+  arc(47 + 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
+
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
   const red = color(196, 0, 0);
