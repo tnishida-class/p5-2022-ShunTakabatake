e83e74625b495b799b44e0235fda18630bb4f088
ShunTakabatake <114969983+ShunTakabatake@users.noreply.github.com>
2022-10-11 12:20:49 +0900

    Update2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,22 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if((i + j) % 2 == 0){
+      fill(255);
+      rect(size*i, size*j, size + size*i, size + size*j); 
+      }
+      else{
+      fill(138, 134, 134);
+      rect(size*i, size*j, size + size*i, size + size*j);
+      if(j < 3){
+        fill(255, 0, 0);
+        ellipse(size*i + size/2,size*j + size/2, size - 3);      
+      }
+      else if(j > 4){
+        fill(0);
+        ellipse(size*i + size/2,size*j + size/2, size - 3); 
+      }
+      }      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
 }
