c5e9960dff0194e2404864c905564340f74e8053
ShunTakabatake <114969983+ShunTakabatake@users.noreply.github.com>
2022-10-11 11:47:05 +0900

    Update 2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
-function setup(){
+function setup() {
+  createCanvas(200,200);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    if(i <= 4){
+      stroke(0, 0, 255);
+
+    }
+    else{
+      stroke(255, 0, 0);
+    }
+    noFill();
+    strokeWeight(1);
+    let x = i*10 + 10
+    ellipse(55, 55, x);
   }
 }
