27d5e035e5e53e5e17b417b056e56edffbebff88
ShunTakabatake <114969983+ShunTakabatake@users.noreply.github.com>
2022-10-21 13:53:13 +0900

    Update 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,7 +1,8 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
-
+const t = 20;
+const a = 1;
 function setup(){
   createCanvas(200, 200);
   count = 0;
@@ -10,7 +11,14 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
+  fill(242, 36, 22)
   count = (count + 1) % cycle;
-  // BLANK[1]
+  let size = 50;
+  if(count <= t){
+    size = size + a*count;
+  }
+  else{
+      size = size - a*t / (count - t);
+  }
   ellipse(width / 2, height / 2, size);
 }
