7aab38f76b51d6170f41da68200274549db62ae5
ShunTakabatake <114969983+ShunTakabatake@users.noreply.github.com>
2022-11-08 11:05:27 +0900

    Updated 5-3

--- section5-3/sketch.js
+++ section5-3/sketch.js
@@ -12,13 +12,27 @@ function setup(){
       console.log(i + "年はうるう年ではありません");
     }
   }
+  //daysInYearの動作確認
+  for(let i = 2000; i <= 2100; i++){
+    console.log(i + "年は" + daysInYear(i) + "日です");
+  }
+
+  //dayOfWeekの動作確認
+  console.log(dayOfWeek(2019, 10, 1))
 }
 
 function calendar(y, m){
   let dow = dayOfWeek(y, m, 1);
+  let l = 0;
   for(let d = 1; d <= daysInMonth(y, m); d++){
     // BLANK[3] (hint: まずは daysInYear, dayOfWeek を作ろう)
+    fill(0);
+    text(d, 20 * dow, 12 * l + 40);
+    if(dow == 6){l += 1}
+    dow = dayOfWeek(y, m, d + 1);
+    text(dayOfWeekAsString(dow), 20 * dow, 25);
   }
+  text(y + "年" + m + "月", 0, 10);  
 }
 
 function isLeapYear(y){
@@ -27,6 +41,7 @@ function isLeapYear(y){
 
 function daysInYear(y){
   // BLANK[1]
+  return isLeapYear(y) ? 366 : 365;
 }
 
 function daysInMonth(y, m){
@@ -51,6 +66,22 @@ function dayOfYear(y, m, d){
 
 function dayOfWeek(y, m, d){
   // BLANK[2]
+  let ydf = abs(2023 - y);
+  let dCount = 0;
+  if(y >= 2023){
+      for(let i = 0; i < ydf; i++){
+      dCount += daysInYear(2023 + i);
+    }
+    dCount += dayOfYear(y, m, d) - 1;
+    return dCount % 7;
+  }
+  else{
+    for(let i = 0; i < ydf; i++){
+      dCount -= daysInYear(2022 - i);
+    }
+    dCount += dayOfYear(y, m, d) - 1;
+    return (7 - abs(dCount) % 7) % 7
+  }  
 }
 
 function dayOfWeekAsString(dow){
